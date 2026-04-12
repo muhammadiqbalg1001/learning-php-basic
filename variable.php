ea5731afe16abe61a82f6c67bbf96bbdc3fe6a02
Muhammad Iqbal Gobel <muhammadiqbal.g1001@gmail.com>
2026-04-12 20:31:07 +0700

    learning variable

--- variable.php
+++ variable.php
@@ -1,7 +1,9 @@
 <?php
 
     $name="Iqbal";
+    $age=24;
 
-    echo "Hello World $name";
+    echo "Hello World! my name $name, i'm $age year's old";
+    echo "<br/> in this year my age is ", $age+1;
 
 ?>
