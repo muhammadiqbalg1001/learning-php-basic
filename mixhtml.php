7d8d6b50f292bde2edaf091979b929d908df0af2
Muhammad Iqbal Gobel <muhammadiqbal.g1001@gmail.com>
2026-04-12 20:59:29 +0700

    learning mix html

---
+++ mixhtml.php
@@ -0,0 +1,24 @@
+<?php
+
+    $name="Iqbal";
+    $age=24;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mis HTML</title>
+</head>
+<body>
+    
+    <h3 style="color: blue;">
+        <?php
+            echo "Hello World! my name is $name and i am $age years old"
+        ?>
+    </h3>
+
+</body>
+</html>
