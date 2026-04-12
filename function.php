0d6106232013214d3cf5c6cc453e052f1d73f838
Muhammad Iqbal Gobel <muhammadiqbal.g1001@gmail.com>
2026-04-12 21:29:44 +0700

    learning function: second function

--- function.php
+++ function.php
@@ -3,10 +3,19 @@
     function penjumlahan() {
         $name="Iqbal";
         $age=24;
+        $total=$age+5;
 
-        echo "Hello World! my name is $name and i am $age years old and in 5 years my age is ", $age+5;
+        echo "Hello World! my name is $name and i am $age years old and in 5 years my age is $total";
+    }
+    
+    function pengurangan() {
+        $age=24;
+        $total=$age-5;
+
+        echo "<br/>in 5 years ago my age is $total";
     }
 
     penjumlahan();
+    pengurangan();
 
 ?>
