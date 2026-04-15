0965f614b826595dd11bbbdebafc8666a41b2ba4
Muhammad Iqbal Gobel <muhammadiqbal.g1001@gmail.com>
2026-04-16 06:15:02 +0700

    learning array and array associative

---
+++ array.php
@@ -0,0 +1,30 @@
+<?php
+
+    // General Array
+
+    $buah = [
+        "Apel",
+        "Jeruk",
+        "Pisang",
+        100,
+        60,
+        true,
+        null,
+        4.5
+    ];
+
+    echo $buah[0];
+    echo $buah[4];
+
+    echo "<br/>";
+
+    // Associative Array
+
+    $student = [
+        "name" => "Iqbal",
+        "age" => 24,
+    ];
+
+    echo $student["name"];
+
+?>
