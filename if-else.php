48cc97baef403c9acef9b3a67fa1a9b5b506455d
muhammadiqbal.g1001 <muhammadiqbal.g1001@gmail.com>
2026-04-26 19:32:11 +0700

    learning if else-if else

--- if-else.php
+++ if-else.php
@@ -2,10 +2,17 @@
 
     $nilai = 80;
 
-    if ($nilai >= 75) {
-        echo "Lulus";
-    } else {
-        echo "Tidak Lulus";
+    if ($nilai >= 90) 
+    {
+        echo "Nilai Kamu A";
+    } 
+    else if($nilai >= 80 && $nilai <= 89) 
+    {
+        echo "Nilai Kamu B";
+    } 
+    else 
+    {
+        echo "Nilai Kamu C";
     }
 
 ?>
