f4383b233b7488c8835f843e89fd7708e578d758
muhammadiqbal.g1001 <muhammadiqbal.g1001@gmail.com>
2026-04-27 17:40:58 +0700

    learning while and while array

---
+++ while.php
@@ -0,0 +1,29 @@
+<?php
+
+    // inisialisasi atau $i di luar, while(kondisi) {isi; $i++}
+
+    // $i = 1;
+
+    // while($i <= 20) {
+    //     echo "Nomor antrian ke-$i <br>";
+    //     $i++;
+    // }
+
+    // echo "<br>";
+
+    // While array
+    $warna = ['merah','biru','hijau','kuning','jingga','biru','hijau','merah','kuning'];
+    $jumlah = 0;
+    $i = 0;
+
+    while($i < count($warna)) {
+        if ($warna[$i] == 'merah') {
+            $jumlah++;
+        }
+        $i++;
+    }
+        
+    echo "Jumlah warna merah adalah $jumlah";
+
+
+?>
