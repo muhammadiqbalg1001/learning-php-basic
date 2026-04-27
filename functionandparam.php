04f3fde28dfd9e0e9b723455974ff55f7ca256ad
muhammadiqbal.g1001 <muhammadiqbal.g1001@gmail.com>
2026-04-27 20:02:29 +0700

    learning function and params

---
+++ functionandparam.php
@@ -0,0 +1,18 @@
+<?php
+
+    function halo($nama) {
+        echo "Halo $nama, apa kabarmu hari ini?";
+    }
+
+    halo("Iqbal");
+
+    echo "<br>";
+
+    function penjumlahan($angka1, $angka2) {
+        $hasil = $angka1 + $angka2;
+        echo $hasil;
+    }
+
+    penjumlahan(5, 7);
+
+?>
